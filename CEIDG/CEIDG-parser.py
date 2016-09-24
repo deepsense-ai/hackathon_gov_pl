cd6cae9c224ca89d4896dfe2d026e65a81d94a56
Król Wszystkiego <jan@milczek.com>
2016-09-24 22:49:24 +0200

    nazwafirmyfixed

--- CEIDG/CEIDG-parser.py
+++ CEIDG/CEIDG-parser.py
@@ -11,12 +11,14 @@ from trans import trans
 
 # In[14]:
 
+skipped = 0
 with open(argv[1] + '.parsed', 'w') as outF:
     with open(argv[1]) as inF:
         for l in inF:
             try:
                 ziom = etree.fromstring(l)
             except etree.XMLSyntaxError:
+                skipped += 1
                 continue
             a1 = ziom.find('DaneAdresowe').find('AdresGlownegoMiejscaWykonywaniaDzialalnosci').find('KodPocztowy').text
             a2 = ziom.find('DaneDodatkowe').find('DataRozpoczeciaWykonywaniaDzialalnosciGospodarczej').text
@@ -24,5 +26,14 @@ with open(argv[1] + '.parsed', 'w') as outF:
             nazwaRaw = unicode(ziom.find('DanePodstawowe').find('Firma').text)
             bezpolskich = trans(nazwaRaw).encode('utf8')
             a4 = ' '.join(filter(lambda x: x.isalpha(), map(lambda x: x if x[-1].isalpha() else x[:-1], bezpolskich.split()))).lower()
-            outF.write(','.join(map(str, [a1,a2,a4,a3])) + '\n')
+            try:
+                outF.write(','.join(map(str, [a1,a2,a4,a3])) + '\n')
+            except UnicodeEncodeError:
+                skipped += 1
+                continue
+
+
+# In[ ]:
+
+print skipped, 'lines skipped'
 
